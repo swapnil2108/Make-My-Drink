92109e13e7b492d37f83aab99ef96f9242a00d4e
Salvi <ssalvi@ssalvi-macmini.carsdirect.win>
2018-01-12 15:05:34 -0800

    changed the header to run in a container for a better look

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -9,19 +9,24 @@ class Header extends React.Component<Props> {
 
   render(){
     return(
-          <div className = "row firstDiv">
-            <div className = "col-12 top-nav">
-              <div className = "text-left">
-                <h6 className="topNavText">Make-My-Drink</h6>
-              </div>
-              <div className = "topNavText text-right">
-                <h6 className="topNavText">About</h6>
-              </div>
-              <div className = "topNavText text-right">
-                <h6 className="topNavText">whats in my bar</h6>
+          <div className = "row sticky">
+            <div className = "col-12 top-nav text-center">
+              <div className = "container-fluid">
+                <div className = "row">
+                  <div className = "col-2 text-left">
+                    <h6 className="topNavText">Make-My-Drink</h6>
+                  </div>
+                  <div className = "col-8 text-right"></div>
+                  <div className = "col-1 text-center">
+                    <a className="topNavTextAbout" href = "#about">About</a>
+                  </div>
+                  <div className = "col-1 text-center">
+                    <h6 className="WIMB">WI<br></br>MB</h6>
+                  </div>
+                </div>
+                </div>
               </div>
             </div>
-          </div>
     );
   }
 }
